792411c6ec1d141037a7fad1ab3511366f6afff0
Manish <nepali.adhikari@gmail.com>
2017-01-27 13:05:41 +0100

    scroll on load done

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,4 +1,16 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealonScroll from './modules/RevealonScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+
+// var revealonScroll = new RevealonScroll();
+
+//for feature items 
+new RevealonScroll($(".feature-item"), "85%");
+
+//for testimonial
+new RevealonScroll($(".testimonial"), "60%");
+
+
 
